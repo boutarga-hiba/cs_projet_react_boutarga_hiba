62fa846d4ac10f888f974596b09cbc3ce4fad8fc
hiba <boutargahiba@gmail.com>
2023-07-26 11:29:25 +0200

    save1

---
+++ src/layouts/Header.js
@@ -0,0 +1,18 @@
+export const Header=()=>
+{
+    return(
+        <>
+            {/* <h1>helo</h1> */}
+            <header>
+            <div className="header">
+
+            </div>
+
+            <nav>
+            <div className="logo"></div>
+            </nav>
+
+            </header>
+        </>
+    )
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../layouts/Header";
+
+export const Home = () => {
+  return (
+    <>
+    <Header/>
+      {/* <h1 className="text-red">Hello to HOME</h1> */}
+    </>
+  );
+};
